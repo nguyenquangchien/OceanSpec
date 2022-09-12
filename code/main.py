36abb4ca518ee91b9040c49d572ed746130d5407
Chien Nguyen <nguyenquangchien@gmail.com>
2022-09-13 00:52:41 +0100

    plotting cosmetics

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import matplotlib
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt 
@@ -17,9 +18,23 @@ def read_file(file_name):
     return ds
 
 
+def move_figure(f, x, y):
+    """ Move the figure window `f` such that the 
+        top left corner is `x` pixels from left edge
+        and `y` pixels from top edge."""
+    backend = matplotlib.get_backend()
+    if backend == 'TkAgg':
+        f.window.wm_geometry(f"+{x}+{y}")
+    elif backend == 'WXAgg':
+        f.window.SetPosition((x, y))
+    else:  # GTK, Qt
+        f.window.move(x, y)
+
+
 def plot_integ(integ):
     """ Plot a map of the integrated wave properties `integ`.
         Parameter: `integ` - a dataset.
+        https://stackoverflow.com/a/26720422/
     """
     map = Basemap(projection='cyl',lon_0=180,lat_0=0,
                   resolution='c', area_thresh = 50, 
@@ -28,12 +43,17 @@ def plot_integ(integ):
     map.drawmeridians(np.arange(0, 361, 30))
     map.drawparallels(np.arange(-90, 91, 30))
 
-    global Hs_array
+    global Hs_array, Dm_array
     Hs_array = integ.swh[0]
-    Hs_array.plot(cmap=plt.cm.coolwarm)
+    Dm_array = integ.mwd[0]
+    ax = Hs_array.plot(cmap=plt.cm.coolwarm)
+    # ax.colorbar(fraction=0.046, pad=0.04)
     # TODO: minimise the color bar # cbar = map.colorbar(shrink=.5, aspect=15, pad=.05)
     plt.connect('motion_notify_event', on_mousemove)
     plt.connect('button_press_event', on_click)
+    plt.tight_layout()
+    thismanager = plt.get_current_fig_manager()
+    move_figure(thismanager, 480, 0)
     plt.show()
 
 
@@ -74,12 +94,13 @@ def on_mousemove(event):
         i = int(-y * 2 + 180)
         j = int(x * 2)
         Hs = Hs_array[i][j].values
+        Dm = Dm_array[i][j].values
         if not np.isnan(Hs):
-            print('Hs = {:.3} m'.format(Hs))
+            print('Hs = {:.3} m,  Dm = {} deg'.format(Hs, Dm))
 
 
 def on_click(event):
-    if event.button == 2:  # right
+    if event.button == 3:  # right
         x, y = event.xdata, event.ydata
         print(f'click at lon {x:.5}, lat {y:.3}')
         
@@ -95,12 +116,14 @@ def on_click(event):
         spectrum = spec_cube[:, :, idx]
         if not precompute:
             spectrum = np.nan_to_num(10**spectrum)
-        polar_plot(spectrum)
+        polar_plot(spectrum, (x, y))
 
 
-def polar_plot(spec_data):
+def polar_plot(spec_data, loca):
     """ Plot the wave spectrum based on the data obtained from `read_spec`.
-        Try imitating https://www.youtube.com/watch?v=DyPjsj6azY4 
+        `spect_data` is an array
+        `loca` is a tuple of long and lat
+        Following https://www.youtube.com/watch?v=DyPjsj6azY4 
         and https://stackoverflow.com/a/9083017/4956603
         https://stackoverflow.com/questions/32462881/add-colorbar-to-existing-axis
         https://stackoverflow.com/questions/15908371/matplotlib-colorbars-and-its-text-labels
@@ -115,17 +138,23 @@ def polar_plot(spec_data):
     n_freq_bins = spec_data.shape[1]
     freqs = np.full(n_freq_bins, FREQ_MIN) * (FACTOR ** np.arange(0, n_freq_bins))
     r, theta = np.meshgrid(freqs, angles)
-
-    fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+    # TODO: inconsistency direction plotted! Please check.
+    fig, ax = plt.subplots(subplot_kw=dict(projection='polar'), figsize=(4,4))
     ctr = ax.contourf(theta, r, spec_data, 10, cmap='OrRd')
+    # ctr = ax.contourf(theta, r, spec_data, 10, cmap='OrRd', vmin=0, vmax=100)
     ax.set_xticklabels(['E', '', 'N', '', 'W', '', 'S', ''])
-    cbar = fig.colorbar(ctr, ax=ax)  # TODO: a unified color scale
+    ax.set_title(f'Wave spectrum at location\nlon {loca[0]:.5}, lat {loca[1]:.3}')
+    cbar = fig.colorbar(ctr, ax=ax)
+    # ax.set_clim(0, 100)
     cbar.set_label('Spectral density, m²s rad⁻¹', rotation=270)
+    thismanager = plt.get_current_fig_manager()
+    move_figure(thismanager, 10, 0)
+    plt.tight_layout()
     fig.show()
 
 
 if __name__ == '__main__':
-    precompute = False
+    precompute = True      # False for system with small memory
     ds = read_file('spect.grib')
     intg = read_file('integ.nc')      # either 'integ.grib' or 'integ.nc'
     print('Finished reading files GRIB (spectra) and netCDF (integral quantities).')
